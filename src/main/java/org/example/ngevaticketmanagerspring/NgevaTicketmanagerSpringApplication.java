0628e62563563223667ce7cb79c1aac0f55e490e
Gagelmann, Martin (Praktikant, GPRA687) <Martin.Gagelmann@gisa.de>
2025-03-03 14:59:02 +0100

    feat(1): Spring-Boot-Projekt erstellt

---
+++ src/main/java/org/example/ngevaticketmanagerspring/NgevaTicketmanagerSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.ngevaticketmanagerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgevaTicketmanagerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NgevaTicketmanagerSpringApplication.class, args);
+    }
+
+}
