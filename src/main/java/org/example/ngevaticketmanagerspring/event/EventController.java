e96b602a1a71ba06f05d2b8caf4762433f3a2191
Gagelmann, Martin (Praktikant, GPRA687) <Martin.Gagelmann@gisa.de>
2025-03-13 17:16:39 +0100

    feat(4): Event create read update delete

---
+++ src/main/java/org/example/ngevaticketmanagerspring/event/EventController.java
@@ -0,0 +1,72 @@
+package org.example.ngevaticketmanagerspring.event;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/event", produces = MediaType.APPLICATION_JSON_VALUE)
+public class EventController {
+
+    private final EventService service;
+
+    @PostMapping
+    public ResponseEntity<Event> createEvent(@Valid @RequestBody Event event) {
+        Event createdEvent = service.createEvent(event);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdEvent);
+    }
+
+    @PutMapping("{eventId}")
+    public ResponseEntity<Object> updateEvent(@PathVariable Long eventId, @Valid @RequestBody Event event) {
+        try {
+            Event updatedEvent = service.updateEvent(eventId, event);
+            return ResponseEntity.ok(updatedEvent);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error: " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEvent(@PathVariable Long id) {
+        try {
+            service.deleteEvent(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Deleted event with ID: " + id + ".");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error: " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteAllEvents() {
+        service.deleteAllEvents();
+        return ResponseEntity.status(HttpStatus.OK).body("Deleted all events.");
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Object> findEventById(@PathVariable Long id) {
+        try {
+          return ResponseEntity.ok(service.findEventById(id));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error: " + e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Event>> getAllEvents() {
+        List<Event> AllEvents = service.getAllEvents();
+        return ResponseEntity.ok(AllEvents);
+    }
+}
